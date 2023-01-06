4c75e12fd63e05bf6aead2c044289a6ce9b5cea7
roteKlaue <janoolhd@gmail.com>
2023-01-07 00:12:47 +0100

    removed unnecessary imports and Collections | switched to cleanContent

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -6,7 +6,7 @@ import play from "../functions/play";
 import userData1 from "../functions/userData";
 
 export default async (_client: Client, message: Message<boolean>) => {
-    if (message.author.bot || !message.content.trim().length || !message.guild?.id) return;
+    if (message.author.bot || !message.cleanContent.trim().length || !message.guild?.id) return;
     const state = guildStates.get(message.guild.id);
 
     if (!state) return;
@@ -14,13 +14,14 @@ export default async (_client: Client, message: Message<boolean>) => {
     const guildData = await guildData1(message.guild.id);
 
     if (!guildData.tts_channel || message.channel.id !== guildData.tts_channel) return;
+    if(message.cleanContent.trim().length >= 200) return message.reply("The maximum amount of characters is 200 per message.");
 
     const userData = await userData1(message.author.id);
 
     let language = userData.language;
     if (!language) language = "en";
 
-    const url = getAudioUrl(message.content, {
+    const url = getAudioUrl(message.cleanContent, {
         lang: language,
         slow: false,
         host: 'https://translate.google.com',
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { connect, set } from 'mongoose';
-import { Client, GatewayIntentBits, Events, Collection } from 'discord.js';
+import { Client, GatewayIntentBits, Events, Collection, Message } from 'discord.js';
 import "dotenv/config";
 import ready from "./events/ready";
 import interaction from "./events/interactionCreate";
 import message from "./events/messageCreate";
 import { commands } from './handlers';
 import Command from './types/Command';
-import { VoiceConnection } from '@discordjs/voice';
-import { ImprovedArray } from 'sussy-util';
 import GuildState from './types/GuildState';
 set('strictQuery', true);
 
@@ -22,25 +20,19 @@ const client = new Client({
 });
 
 const command = new Collection<string, Command>();
-const channels = new Collection<string, VoiceConnection>();
-const queues = new Collection<string, ImprovedArray<string>>();
 
 const guildStates = new Collection<string, GuildState>();
 
 commands(command);
 
 client.once(Events.ClientReady, ready.bind(null, client, command));
-client.on(Events.InteractionCreate, (interactions) => {
-    interaction(client, command, interactions)
-});
-client.on(Events.MessageCreate, message.bind(null, client));
+client.on(Events.InteractionCreate, (interactions) => { interaction(client, command, interactions) });
+client.on(Events.MessageCreate, (msg: Message<boolean>) => { message(client, msg) });
 
 client.login(process.env.TOKEN);
 connect(process.env.MONGO || "");
 
 export {
-    channels,
     command,
-    queues,
     guildStates
 }
